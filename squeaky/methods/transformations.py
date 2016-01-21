374ee82e462b1beaf5a3140b036f8d28dba3dd5d
Sahil Singh <sahilsingh90@gmail.com>
2016-01-21 16:39:52 +0530

    Added categorization transformation

--- squeaky/methods/transformations.py
+++ squeaky/methods/transformations.py
@@ -5,8 +5,13 @@ def categorize(column, max_val, min_val, binwidth):
         and return the mapping of values to bins"""
     #max_val = max(column)
     #min_val = min(column)
-    labels = ["{0} - {1}".format(i, (i + binwidth - 1)) for i in range(min_val, max_val + binwidth, binwidth)]
-    groups = pd.cut(column, range(min_val-1, max_val + binwidth*2, binwidth), right=True, labels=labels)
+    labels = [ "{0} - {1}".format(i, (i + binwidth - 1))
+                for i in range(min_val, max_val + binwidth, binwidth)
+             ]
+    groups = pd.cut(column,
+                    range(min_val-1, max_val + binwidth*2, binwidth),
+                    right=True,
+                    labels=labels)
     zipped = {}
     for n, entry in enumerate(column):
         zipped[entry] = groups[n]
